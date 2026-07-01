67905f628c3a54e12c1b1c6c230d708c6dc11779
Rockywell <P8errockwell@gmail.com>
2026-06-30 23:57:12 -0600

    implemented my code and plan changes for week 1

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,21 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // To get the numbers of multiples you define an empty fixed array the size of the length variable
+        // You then create a for loop that runs/loops the amount of times equal to the length variable.
+        // Your iterator varaible (e.g. i) will start at 0.
+        // The incrementer value plus one (e.g. i + 1) will accurately represent the number of iterated loops. 
+        // During the loop you will multiply the number by the index/counter (e.g. i) of the loop.
+        // You take the resulting number and push/add/assign it the array that stores your multiples.
+        // (e.g. multiples[i] = multipleResult)
+        // At the end of the entire for loop you will return the array of multiples as your result.
+
+        double[] multiples = new double[length];
+
+        for (int i = 0; i < length; i++)
+            multiples[i] = number * (i + 1);
+
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +43,26 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // The amount should be reassigned into a remainder of the length. (OPTIONAL)
+        // (This handles cases where amount is greater than length)
+        // First get the length/count of the data variable and minus it by the amount variable.
+        // Use the result as the index of where to get the data variable/list into two parts.
+        // Append/combine the two parts of the list with the ordering of the parts reversed.
+        // (The first/beginning part of the list becomes the end and the second becomes the beginning.)
+        // This result should be assigned to and permanently modify the data variable/list.
+
+        int length = data.Count;
+        amount %= length;
+
+        int index = length - amount;
+
+        // Concat makes it a generic enumerable, so we make sure its back to a list with ToList().
+        List<int> rotatedData = data.Skip(index)
+                                    .Concat(data.Take(index))
+                                    .ToList();
+
+        data.Clear();
+        data.AddRange(rotatedData);
     }
 }
